4ede2936afbe485a4c87a11642c6e05c49e5b4c6
PENG-AO <71684142+PENG-AO@users.noreply.github.com>
2021-03-25 17:47:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from emulator import Emulator
 
-ROM = './test_rom/cpu_instr/11.gb'
-# ROM = './test_rom/games/opus5.gb'
+ROM = './romfile.gb'
 emu = Emulator(ROM, skipBios=True, nostalgic=True)
-'''
-# add a sprite info
-emu.write(0xFE00, 0x54)
-emu.write(0xFE01, 0x54)
-emu.write(0xFE02, 0x02)
-emu.write(0xFE03, 0x00)
-'''
 emu.run()
